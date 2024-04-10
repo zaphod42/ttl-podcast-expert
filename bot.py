ec38e832d4d3b12abdcf4cd931c4363c5d42e566
Andrew Parker <aparker42@gmail.com>
2024-04-10 16:37:34 +0100

    Give the main file a better name

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core import VectorStoreIndex, Settings
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.llms.ollama import Ollama
 from llama_index.readers.web import RssReader
 
-#documents = SimpleDirectoryReader("data").load_data()
 documents = RssReader(html_to_text=True).load_data([f'https://thettlpodcast.com/feed/?paged={p}' for p in range(1, 10)])
 print(f"Loaded extra documents ({len(documents)})")
 
